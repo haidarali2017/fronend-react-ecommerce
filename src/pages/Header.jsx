937409b5e39f499e92ca99c451912cbd6cd6c16e
haidarcse2018 <haidarcse2017@gmail.com>
2023-06-22 01:31:46 +0600

    dynamic route

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -6,9 +6,10 @@ import {Link} from 'react-router-dom';
 function Header() {
  
     return (
-      <Navbar bg="light" expand="lg">
+      
+      <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="">eccomerce</Navbar.Brand>
+        <Navbar.Brand href="">Softstation71</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
